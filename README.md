b4b1a98b5b6af49256641f308903a7a45c4fbf13
hiant <heihuwudi@gmail.com>
2016-05-27 14:39:53 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # gitbook-watcher
+
+## 简介
+
+gitbook-watcher 是一个用 golang 实现的 gitbook 本地辅助工具。项目地址：[https://github.com/hiant/gitbook-watcher](https://github.com/hiant/gitbook-watcher)
+
+gitbook本身自带的livereload，使用nodejs做的监控。对于某些内存比较小的机器来说，本地开发一段时间，多次保存触发livereload之后，node应用的
+
+## 安装编译
+
+1. 安装并部署go
+2. 使用 `git clone https://github.com/hiant/gitbook-watcher.git` 检出项目或者在项目主页下载 [zip压缩包](https://codeload.github.com/hiant/gitbook-watcher/zip/master) 解压后，进到目录执行 go build watcher.go 进行编译
+3. 项目中包含在win7x64环境下编译的可执行程序，应该可以直接使用
+
+## 使用
+
+将可执行程序放到你的book下，和book.json平级。
+  ```
+  Usage of watcher:
+    -path string
+          Watcher path (default ".")
+    -port string
+          Listening port (default "4000")
+  ```
---
+++ watcher.go
@@ -0,0 +1,204 @@
+package main
+
+import (
+	"crypto/md5"
+	"expvar"
+	"flag"
+	"fmt"
+	"github.com/fsnotify/fsnotify"
+	"github.com/hiant/go-shutil"
+	"github.com/valyala/fasthttp"
+	"github.com/valyala/fasthttp/expvarhandler"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+func main() {
+
+	path := flag.String("path", ".", "Watcher path")
+	port := flag.String("port", "4000", "Listening port")
+
+	flag.Parse()
+
+	watcher, err := fsnotify.NewWatcher()
+	check(err)
+	defer watcher.Close()
+
+	// Setup FS handler
+	root := *path
+	root += "/.website"
+	os.RemoveAll(root)
+	err = os.MkdirAll(root, os.ModePerm)
+	check(err)
+
+	fs := &fasthttp.FS{
+		Root:               root,
+		IndexNames:         []string{"index.html"},
+		GenerateIndexPages: true,
+		Compress:           false,
+		AcceptByteRange:    false,
+		CacheDuration:      60 * time.Second,
+	}
+	fsHandler := fs.NewRequestHandler()
+
+	requestHandler := func(ctx *fasthttp.RequestCtx) {
+		switch string(ctx.Path()) {
+		case "/stats":
+			expvarhandler.ExpvarHandler(ctx)
+		default:
+			fsHandler(ctx)
+			updateFSCounters(ctx)
+		}
+	}
+
+	// Start HTTP server.
+	addr := fmt.Sprintf("0.0.0.0:%s", *port)
+	log.Printf("Starting HTTP server on %s", addr)
+
+	options := &shutil.CopyTreeOptions{Symlinks: false,
+		Ignore:                 nil,
+		CopyFunction:           shutil.Copy,
+		IgnoreDanglingSymlinks: false,
+		OnlySubDir:             true}
+
+	go gitbookBuild(*path, options)
+
+	go func() {
+		if err := fasthttp.ListenAndServe(addr, requestHandler); err != nil {
+			log.Fatalf("error in ListenAndServe: %s", err)
+		}
+	}()
+
+	log.Printf("Serving files from directory [%s]", root)
+	log.Printf("See stats at http://%s/stats", addr)
+
+	file2sum := map[string]string{}
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				_, file := filepath.Split(event.Name)
+				if file[0] != '.' && !strings.EqualFold(file, "_book") {
+					if fileInfo, err := os.Stat(event.Name); err == nil {
+						var sum string
+						if fileInfo.IsDir() {
+							sum = fmt.Sprint("%x", md5.Sum([]byte(event.Name)))
+						} else {
+							sum = md5file(event.Name)
+						}
+						oldSum, has := file2sum[event.Name]
+						if !has || !strings.EqualFold(oldSum, sum) {
+							log.Println("event:", event)
+							file2sum[event.Name] = sum
+							gitbookBuild(*path, options)
+						}
+					}
+				}
+			case err := <-watcher.Errors:
+				panic(err)
+			}
+		}
+	}()
+
+	pwd, _ := filepath.Abs(".")
+	err = watcher.Add(pwd)
+	check(err)
+	log.Printf("Directory [%v] was watched", pwd)
+	addWatcher(watcher, ".")
+	<-done
+}
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+		panic(e)
+	}
+}
+
+func addWatcher(watcher *fsnotify.Watcher, path string) error {
+	dir, err := ioutil.ReadDir(path)
+	check(err)
+	for _, fd := range dir {
+		if fd.IsDir() {
+			subPath := fd.Name()
+			prefix := subPath[0]
+			if prefix == '.' || prefix == '_' || strings.HasPrefix(subPath, "node_modules") {
+				continue
+			}
+			relativePath := filepath.Join(path, subPath)
+			pwd, _ := filepath.Abs(relativePath)
+			err = watcher.Add(relativePath)
+			if err == nil {
+				log.Printf("Directory [%v] was watched", pwd)
+			}
+			err = addWatcher(watcher, relativePath)
+		}
+	}
+	return err
+}
+
+func gitbookBuild(path string, options *shutil.CopyTreeOptions) {
+	build := exec.Command("gitbook", "build")
+	build.Dir = path
+	build.Stdout = os.Stdout
+	build.Stderr = os.Stderr
+	err := build.Run()
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	} else {
+		err = shutil.CopyTree("_book", ".website", options)
+		check(err)
+		log.Printf("Gitbook rebuild success")
+	}
+}
+
+func md5file(path string) string {
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	content, err := ioutil.ReadAll(file)
+	return fmt.Sprint("%x", md5.Sum(content))
+}
+
+func updateFSCounters(ctx *fasthttp.RequestCtx) {
+	// Increment the number of fsHandler calls.
+	fsCalls.Add(1)
+
+	// Update other stats counters
+	resp := &ctx.Response
+	switch resp.StatusCode() {
+	case fasthttp.StatusOK:
+		fsOKResponses.Add(1)
+		fsResponseBodyBytes.Add(int64(resp.Header.ContentLength()))
+	case fasthttp.StatusNotModified:
+		fsNotModifiedResponses.Add(1)
+	case fasthttp.StatusNotFound:
+		fsNotFoundResponses.Add(1)
+	default:
+		fsOtherResponses.Add(1)
+	}
+}
+
+// Various counters - see https://golang.org/pkg/expvar/ for details.
+var (
+	// Counter for total number of fs calls
+	fsCalls = expvar.NewInt("fsCalls")
+
+	// Counters for various response status codes
+	fsOKResponses          = expvar.NewInt("fsOKResponses")
+	fsNotModifiedResponses = expvar.NewInt("fsNotModifiedResponses")
+	fsNotFoundResponses    = expvar.NewInt("fsNotFoundResponses")
+	fsOtherResponses       = expvar.NewInt("fsOtherResponses")
+
+	// Total size in bytes for OK response bodies served.
+	fsResponseBodyBytes = expvar.NewInt("fsResponseBodyBytes")
+)
